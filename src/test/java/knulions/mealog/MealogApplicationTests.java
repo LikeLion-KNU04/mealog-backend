f5c828b0c46e1df81179cafb7110ab342e725844
Gyuri <haha0888@naver.com>
2024-07-14 22:34:55 -0700

    init 초기화

---
+++ src/test/java/knulions/mealog/MealogApplicationTests.java
@@ -0,0 +1,13 @@
+package knulions.mealog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
